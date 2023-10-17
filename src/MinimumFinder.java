ada7e97e1bbe8ef6bba861b9721f4c6503327f6a
danielbanmeke <banmekedaniel@gmail.com>
2023-10-17 09:06:46 +0100

    Update all files

---
+++ src/MinimumFinder.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MinimumFinder {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter 3 floating-point values separated by spaces: ");
+        double number1 = input.nextDouble();
+        double number2 = input.nextDouble();
+        double number3 = input.nextDouble();
+
+        double result = minimum(number1, number2, number3);
+
+        System.out.printf("Minimum is ", + result);
+    }
+
+    public static double minimum (double x, double y, double z){
+        double minimumValue = x;
+
+        if (y < minimumValue)
+            minimumValue = y;
+
+        if (z < minimumValue)
+            minimumValue = z;
+
+        return minimumValue;
+
+    }
+
+}
