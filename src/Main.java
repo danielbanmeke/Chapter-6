0726600264a18dc00f7b5b64760992666eeaa664
danielbanmeke <banmekedaniel@gmail.com>
2023-10-17 19:32:39 +0100

    Update all files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Enter number1 op number2 (5 * 7): ");
+        Scanner input = new Scanner(System.in);
+        double number1 = input.nextDouble();
+        char op = input.next().charAt(0);
+        double number2 = input.nextDouble();
+
+        if ( op == '+')
+            System.out.println(number1 + number2);
+        else if (op == '-')
+            System.out.println(number1 - number2);
+        else if (op == '/')
+            System.out.println(number1 / number2);
+        else if (op == '*')
+            System.out.println(number1 * number2);
     }
 }
