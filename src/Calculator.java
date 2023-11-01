c4c857fb49e21e1a95b4ab74f7a6c576ac68d50b
danielbanmeke <banmekedaniel@gmail.com>
2023-11-01 14:04:38 +0100

    Update all files

---
+++ src/Calculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter num1 operator num2 (example: 1 + 2): ");
+
+        double d1 = input.nextDouble();
+        char op = input.next().charAt(0);
+        double d2 = input.nextDouble();
+
+        if (op == '+')
+            System.out.println(d1 + d2);
+        else if (op == '-')
+            System.out.println(d1 - d2);
+        else if (op == '*')
+            System.out.println(d1 * d2);
+        else if (op == '/')
+            System.out.println(d1 / d2);
+    }
+}
